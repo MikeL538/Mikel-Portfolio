09d3463131a35459ee54eda1559a9f43fbfa60d1
MikeL538 <126614886+MikeL538@users.noreply.github.com>
2023-08-03 22:18:37 +0200

    Update
    Added Polish translation and fixed some minor bugs

---
+++ js/lagnuage-selector.js
@@ -0,0 +1,138 @@
+const translations = {
+  en: {
+    home: "Home",
+    about: "About",
+    projects: "Projects",
+    contact: "Contact",
+    welcome: "Hi!",
+    welcome2: "My name is",
+    technologies: "Technologies and Tools",
+    technologiesWorked: "Technologies I've worked with",
+    recentProjects: "Recent projects",
+    projectDesc1:
+      "This website is a representation of a fictional company. It marks a significant milestone for me as it is the first project I have developed independently, without relying on Youtube tutorials or other step-by-step materials. This project was completed as part of my Fullstack Frontend Developer course organized by GOIT.",
+    projectDesc2:
+      "The pack of tasks showcasing my skills in JavaScript programming. These tasks were originally prepared by GOIT for their course, which I have successfully completed and then improved upon in this project.",
+    projectDesc3:
+      'This website showcases a fictional florist named "Anemone." Originally a team project by GOIT, we successfully completed it together. However, I wanted to further enhance my skills, so I took the initiative to work on this project independently as well, in addition to the teamwork aspect.',
+    education: "Education",
+    education1Span1: "Bachelor of English Philology",
+    education1Span2: "Wyższa Szkoła Filologiczna in Wrocław",
+    education2Span1: "Master of English Philology",
+    education2Span2: "University of Szczecin",
+    skills: "Skills",
+    careerObjectiveH: "Career Objective",
+    careerObjective:
+      'I am passionately pursuing a career in the IT industry after discovering my love for programming in September 2022. Since then, I have dedicated myself to learning various programming languages and tools. I am participant of the "Fullstack Frontend Developer" course by GOIT. I have acquired proficiency in HTML, CSS, SASS (SCSS), JavaScript and I am currently deepening my knowledge of NodeJS and React. In addition to programming languages, I have also familiarized myself with essential technologies and tools related to frontend development. I have gained experience in using platforms such as Github, Figma, Squoosh, IcoMoon, SVGOMG, and much more. My journey into the IT field has been an exciting and enriching experience, and I am eager to continue expanding my skills and knowledge in programming and frontend development.',
+    workExp: "Work Experience",
+    workExpE1: "English teacher in primary school - 2022-2023",
+    workExpE2:
+      'English teacher at the private kindergarten "Stacyjkowo" in Żagań - 2021-2023',
+    workExpE3: "English teacher in primary school - 2021-2022",
+    workExpE4: "Assistant of designer - 2017-2020",
+    finishedProjects: "Finished Projects",
+    project1:
+      "This website represents a fictional company and holds great significance for me. It marks a significant milestone as it is my first independent project, without relying on Youtube tutorials or other step-by-step resources. I successfully completed this project during my Fullstack Frontend Developer course at GOIT.",
+    project2:
+      "The pack of tasks showcasing my skills in JavaScript programming. These tasks were originally prepared by GOIT for their course, which I have successfully completed and then improved upon in this project.",
+    project3:
+      'This website showcases a fictional florist named "Anemone". Originally a team project by GOIT, we successfully completed it together. However, I wanted to further enhance my skills, so I took the initiative to work on this project independently as well, in to the teamwork aspect.',
+    project4:
+      "This website is dedicated to a fictional fitness company that caught my attention on Figma. I made some tweaks to the original design and introduced a modal window feature. Below the button, you'll find a link to the original Figma design.",
+    project5:
+      "After watching a video on YouTube showcasing a two-sided portfolio, I was inspired to create my own version using a credit card design. I implemented a click event that triggers a flip effect, resulting in an interactive experience where the bank card flips.",
+    projectsInProgress: "Projects in progress",
+  },
+  pl: {
+    home: "Strona główna",
+    about: "O mnie",
+    projects: "Projekty",
+    contact: "Kontakt",
+    welcome: "Hej!",
+    welcome2: "Nazywam się",
+    technologies: "Technologie i narzędzia",
+    technologiesWorked: "Technologie z którymi pracowałem",
+    recentProjects: "Ostatnie projekty",
+    projectDesc1:
+      "Ta strona internetowa jest reprezentacją fikcyjnej firmy. Jest to ważny kamień milowy dla mnie, ponieważ jest to pierwszy projekt, który stworzyłem samodzielnie, bez polegania na poradnikach z YouTube ani innych krok po kroku materiałach. Ten projekt został ukończony w ramach mojego kursu Fullstack Frontend Developer organizowanego przez GOIT.",
+    projectDesc2:
+      "Jest to zestaw zadań prezentujących moje umiejętności programowania w języku JavaScript. Te zadania zostały pierwotnie przygotowane przez GOIT na potrzeby ich kursu, który ukończyłem z sukcesem, a następnie ulepszyłem w ramach tego projektu.",
+    projectDesc3:
+      'Ta strona internetowa prezentuje fikcyjną kwiaciarnię o nazwie "Anemone". Początkowo był to projekt zespołowy w ramach GOIT, który ukończyliśmy razem z sukcesem. Jednak chciałem dalej rozwijać swoje umiejętności, dlatego postanowiłem również samodzielnie stworzyć ten projekt.',
+    education: "Edukacja",
+    education1Span1: "Licencja filologii angielskiej",
+    education1Span2: "Wyższa Szkoła Filologiczna we Wrocławiu",
+    education2Span1: "Magister filologii angielskiej",
+    education2Span2: "Uniwersytet Szczeciński",
+    skills: "Umiejętności",
+    careerObjectiveH: "Cele Zawodowe",
+    careerObjective:
+      'Z pasją dążę do kariery w branży IT po odkryciu mojej miłości do programowania we wrześniu 2022 roku. Od tego czasu poświęciłem się nauce różnych języków programowania i narzędzi. Jestem uczestnikiem kursu "Fullstack Frontend Developer" organizowanego przez GOIT. Zdobyłem biegłość w HTML, CSS, SASS (SCSS), JavaScript oraz obecnie pogłębiam swoją wiedzę z zakresu NodeJS i React. Oprócz języków programowania, zaznajomiłem się także z niezbędnymi technologiami i narzędziami związanymi z rozwojem frontendu. Posiadam doświadczenie w korzystaniu z platform takich jak Github, Figma, Squoosh, IcoMoon, SVGOMG i wiele innych. Moja przygoda z branżą IT była ekscytującym i wzbogacającym doświadczeniem, i z niecierpliwością kontynuuję rozwijanie swoich umiejętności i wiedzy w programowaniu i rozwoju frontendu.',
+    workExp: "Doświadczenie Zawodowe",
+    workExpE1: "Nauczyciel angielskiego w szkole podstawowej - 2022-2023",
+    workExpE2:
+      'Nauczyciel angielskiego w prywatnym przeszkolu "Stacyjkowo" - 2021-2023',
+    workExpE3: "Nauczyciel angielskiego w szkole podstawowej - 2021-2022",
+    workExpE4: "Asystant designera - 2017-2020",
+    finishedProjects: "Ukończone Projekty",
+    project1:
+      "Ta strona internetowa przedstawia fikcyjną firmę i ma dla mnie ogromne znaczenie. To ważny kamień milowy, ponieważ jest to mój pierwszy samodzielny projekt, bez korzystania z poradników na YouTube ani innych krok po kroku materiałów. Pomyślnie ukończyłem ten projekt podczas kursu Fullstack Frontend Developer w GOIT.",
+    project2:
+      "Jest to zestaw zadań prezentujących moje umiejętności programowania w języku JavaScript. Te zadania zostały pierwotnie przygotowane przez GOIT na potrzeby ich kursu, który ukończyłem z sukcesem, a następnie ulepszyłem w ramach tego projektu.",
+    project3:
+      'Ta strona internetowa prezentuje fikcyjną kwiaciarnię o nazwie "Anemone". Początkowo był to projekt zespołowy w ramach GOIT, który ukończyliśmy razem z sukcesem. Jednak chciałem dalej rozwijać swoje umiejętności, dlatego postanowiłem również samodzielnie stworzyć ten projekt.',
+    project4:
+      "Ta strona internetowa jest poświęcona fikcyjnej firmie fitness, która przyciągnęła moją uwagę na platformie Figma. Wprowadziłem pewne zmiany w oryginalnym projekcie i dodałem funkcję okna modalnego. Poniżej przycisku znajdziesz link do oryginalnego projektu na platformie Figma.",
+    project5:
+      "Po obejrzeniu filmu na YouTube, w którym przedstawiono dwustronne portfolio, zainspirowałem się do stworzenia swojej własnej wersji wykorzystującej projekt karty kredytowej. Wprowadziłem zdarzenie kliknięcia, które wywołuje efekt odwracania, co daje interaktywne doświadczenie, gdzie karta bankowa się obraca.",
+    projectsInProgress: "Projekty w trakcie realizacji",
+  },
+};
+
+const enLanguageButton = document.querySelector("#en-language");
+const plLanguageButton = document.querySelector("#pl-language");
+const elementsToTranslate = document.querySelectorAll("[data-translate]");
+
+let currentLanguage = localStorage.getItem("language") || "en";
+
+function translateElements(language) {
+  elementsToTranslate.forEach((element) => {
+    const translationKey = element.dataset.translate;
+    element.textContent = translations[language][translationKey];
+  });
+}
+
+const switchLanguage = (language) => {
+  currentLanguage = language;
+  localStorage.setItem("language", language);
+  translateElements(currentLanguage);
+};
+
+function langCheck() {
+  if (currentLanguage === "en") {
+    enLanguageButton.style.display = "none";
+    plLanguageButton.style.display = "block";
+  }
+
+  if (currentLanguage === "pl") {
+    plLanguageButton.style.display = "none";
+    enLanguageButton.style.display = "block";
+  }
+}
+
+plLanguageButton.addEventListener("click", () => {
+  if (currentLanguage !== "pl") {
+    switchLanguage("pl");
+    langCheck();
+  }
+});
+
+enLanguageButton.addEventListener("click", () => {
+  if (currentLanguage !== "en") {
+    switchLanguage("en");
+    langCheck();
+  }
+});
+
+translateElements(currentLanguage);
+langCheck();
